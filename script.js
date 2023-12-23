d340153ec6e69ab7e15840491f66382198602ac0
CherikchievTimur <fquantu@gmail.com>
2023-12-23 09:13:26 -0600

    Rewrite all project

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const genderImg = document.querySelector('#genderImg');
-const male = document.querySelector('.male');
-const female = document.querySelector('.female');
+const female = document.querySelector(".female");
+const male = document.querySelector(".male");
+
+const percentFemale = document.querySelector(".percent-female");
+const percentMale = document.querySelector(".percent-male");
 
 let nameResearch = ''
 
@@ -14,20 +16,20 @@ const getData = async (url) => {
 const onChangeName = (e) => nameResearch = e.target.value;
 
 const onClick = async () => {
-    const url = `https://api.genderize.io?name=${nameResearch}`    
+    const url = `https://api.genderize.io?name=${nameResearch}`
 
-    const data = await getData(url)
+    const data = await getData(url);
 
-    console.log(data)
+    redrawImages(calcGenderPercentage(data));
 
-    let malePersent = 100;
-    let femalePersent = 100;
+    /*let malePersent = .5;
+    let femalePersent = .5;
 
-    switch(data.gender){
-        case 'male':{
+    switch (data.gender) {
+        case 'male': {
             malePersent = data.probability;
             femalePersent = 1 - malePersent;
-        }break;
+        } break;
         case 'female': {
             femalePersent = data.probability;
             malePersent = 1 - femalePersent;
@@ -37,6 +39,58 @@ const onClick = async () => {
     console.log(malePersent, femalePersent);
 
     male.style.height = `${malePersent * 100}%`;
-    female.style.height = `${femalePersent * 100}%`;
-
+    female.style.height = `${femalePersent * 100}%`;*/
 }
+
+const calcGenderPercentage = (data) => {
+    const genderPercentage = {
+        gender: data.gender,
+        female: 0.5,
+        male: 0.5
+    }
+
+    switch (genderPercentage.gender) {
+        case 'male': {
+            genderPercentage.male = data.probability;
+            genderPercentage.female = 1 - genderPercentage.male;
+        } break;
+        case 'female': {
+            genderPercentage.female = data.probability;
+            genderPercentage.male = 1 - genderPercentage.female;
+        } break;
+    }
+
+    return genderPercentage;
+}
+
+const redrawImages = (genderPercentage) => {
+    let pMale = genderPercentage.male;
+    let pFemale = genderPercentage.female;
+
+    if (pMale && pMale <= .1) {
+        pMale = .15;
+        pFemale = .85;
+    }
+    else if (pFemale && pFemale <= .1) {
+        pMale = .85;
+        pFemale = .15;
+    }
+
+    const percentageFemale = pFemale * 100;
+    const percentageMale = pMale * 100;
+
+    const femaleBounding = female.getBoundingClientRect();
+
+    female.style.clipPath = `inset(0px ${percentageMale}% 0px 0px)`;
+    male.style.clipPath = `inset(0px 0px 0px ${percentageFemale}%)`;
+
+    if (genderPercentage.female) {
+        percentFemale.textContent = (genderPercentage.female * 100).toFixed(0);
+        percentFemale.style.left = `${femaleBounding.left + femaleBounding.width * pFemale - 40}px`
+    }
+    if (genderPercentage.male) {
+        percentMale.textContent = (genderPercentage.male * 100).toFixed(0);
+        percentMale.style.left = `${femaleBounding.left + femaleBounding.width * pFemale + 20}px`
+    }
+}
+
